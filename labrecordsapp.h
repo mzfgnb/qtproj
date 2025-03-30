8e5296b9331b7fe24d7f959693f06ee52631e829
mzfgnb <120786156+mzfgnb@users.noreply.github.com>
2025-03-30 20:38:30 +0300

    Add files via upload

---
+++ labrecordsapp.h
@@ -0,0 +1,23 @@
+#ifndef LABRECORDSAPP_H
+#define LABRECORDSAPP_H
+
+#include <QMainWindow>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+
+class LabRecordsApp : public QMainWindow {
+    Q_OBJECT
+public:
+    explicit LabRecordsApp(QWidget *parent = nullptr);
+private slots:
+    void addRecord();
+    void editRecord();
+    void deleteRecord();
+    void saveToFile();
+    void loadFromFile();
+
+private:
+    QTableWidget *tableWidget;
+};
+
+#endif
